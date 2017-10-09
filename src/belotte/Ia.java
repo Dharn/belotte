9d055a30e11d70b876493580170dd4e3605b686b
Nicolas Rongieras <nrongier@ensc.fr>
2017-10-09 10:21:39 +0200

    choix atout choix atout couleur

--- src/belotte/Ia.java
+++ src/belotte/Ia.java
@@ -1,6 +1,8 @@
 package belotte;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Ia {
@@ -16,7 +18,7 @@ public class Ia {
 
 		if (pointsMainSiAtout(main, laRetourne) >= 24) {
 			Random r = new Random();
-			float point = (float) pointsMainSiAtout(main, laRetourne) + (float) r.nextInt(21);
+			float point = (float) pointsMainSiAtout(main, laRetourne);
 			float pourcentage = (point * 100) / 44;
 			if (r.nextFloat() * 100 < pourcentage) {
 				return " oui";
@@ -29,8 +31,49 @@ public class Ia {
 		}
 	}
 
-	public Carte choisirAtoutCouleur(ArrayList<Carte> main) {
-		return null;
+	public Couleur choisirAtoutCouleur(ArrayList<Carte> main) {
+
+		ArrayList<Couleur> couleursCartes = new ArrayList<Couleur>();
+		Map<Couleur, ArrayList<Carte>> couleursmain = new HashMap<Couleur, ArrayList<Carte>>();
+		couleursmain.put(Couleur.carreau, new ArrayList<Carte>());
+		couleursmain.put(Couleur.coeur, new ArrayList<Carte>());
+		couleursmain.put(Couleur.pique, new ArrayList<Carte>());
+		couleursmain.put(Couleur.trèfle, new ArrayList<Carte>());
+
+		for (Carte carte : main) {
+			for (Couleur couleur : Couleur.values()) {
+				if (carte.getCouleur() == couleur) {
+					couleursmain.get(couleur).add(carte);
+				}
+			}
+		}
+
+		Couleur couleurPlusForte = null;
+		int pointsCouleurPlusForte = 0;
+
+		for (Couleur couleur : Couleur.values()) {
+			Carte couleuretourne = couleursmain.get(couleur).get(0);
+			couleursmain.get(couleur).remove(0);
+			int points = pointsMainSiAtout(couleursmain.get(couleur), couleuretourne);
+			if (points > pointsCouleurPlusForte) {
+				pointsCouleurPlusForte = points;
+				couleurPlusForte = couleur;
+			}
+		}
+		
+		if (pointsCouleurPlusForte >= 24) {
+			Random r = new Random();
+			float point = (float) pointsCouleurPlusForte;
+			float pourcentage = (point * 100) / 44;
+			if (r.nextFloat() * 100 < pourcentage) {
+				return couleurPlusForte;
+			} else {
+				return null;
+			}
+		}
+		else {
+			return null;
+		}
 	}
 
 	public Carte choisirCarteAPoser(Pli pli, ArrayList<Carte> main) {
